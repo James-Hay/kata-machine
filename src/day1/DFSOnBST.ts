2855ce9e2564c647ec54c04a7ed1a989c2d427e5
James Hay <madsaber@gmail.com>
2024-04-01 21:58:13 +0100

    yayayayaya

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,23 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function traverse(head: BinaryNode<number> | null, needle: number): boolean {
+    if (!head) {
+        return false;
+    }
 
+    if (head?.value === needle) {
+        return true;
+    }
+
+    if (head.value < needle) {
+        return traverse(head.right, needle);
+    }
+
+    if (head.value >= needle) {
+        return traverse(head.left, needle);
+    }
+
+    return false;
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return traverse(head, needle);
 }
